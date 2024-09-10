9be0ad6b6970bb58e8da8acdcb9154d502590307
Mirco Santori <mirco.santori@roche.com>
2024-09-10 14:41:45 +0200

    fix(output): remove unecessary output from eks module

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "endpoint" {
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
-
-output "sgid" {
-  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
-}
-
-output "eks" {
-  value = aws_eks_cluster.this
-}
